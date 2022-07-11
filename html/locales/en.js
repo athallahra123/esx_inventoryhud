b3da02a1541ec83e9f179869ca7e6d9a189cc28e
athallahra123 <102503406+athallahra123@users.noreply.github.com>
2022-07-11 20:15:42 +0700

    Add files via upload

---
+++ html/locales/en.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Destroy";
+invLocale.useItem = "Gunakan";
+invLocale.giveItem = "Berikan";
+invLocale.secondInventoryNotAvailable = "Second inventory is not available";
---
+++ html/locales/es.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "DESTROY";
+invLocale.useItem = "Use";
+invLocale.giveItem = "Give";
+invLocale.secondInventoryNotAvailable = "";
